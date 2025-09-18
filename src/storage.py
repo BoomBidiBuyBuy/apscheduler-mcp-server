6cbb71dad14fdb9587de659247539ea5f4ca5bde
TanyaKovalenko <tanyakovalenko1994@gmail.com>
2025-09-18 22:50:38 +0300

    after formating

--- src/storage.py
+++ src/storage.py
@@ -1,5 +1,6 @@
 import envs
 
+
 def get_database_url() -> str:
     if envs.STORAGE_DB.startswith("sqlite"):
         if envs.STORAGE_DB == "sqlite-memory":
@@ -11,13 +12,14 @@ def get_database_url() -> str:
     else:
         raise ValueError("The `STORAGE_DB` env variable is not correct")
 
+
 def get_scheduler_jobstore():
     database_url = get_database_url()
-    
+
     return {
-        'default': {
-            'type': 'sqlalchemy',
-            'url': database_url,
-            'tablename': 'apscheduler_jobs'
+        "default": {
+            "type": "sqlalchemy",
+            "url": database_url,
+            "tablename": "apscheduler_jobs",
         }
     }
